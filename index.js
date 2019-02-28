58475552e9855bd74b0e95946e14cd0113bd1a42
Luke Bennett <lukeeeebennettplus@gmail.com>
2019-02-28 22:22:00 +0000

    A wild commit appeared.

---
+++ index.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const stringFormatter = require("stylelint/lib/formatters/stringFormatter");
+
+// borrowed from stringFormatter as it isn't exported yet
+function logFrom(fromValue) {
+  if (fromValue.charAt(0) === "<") return fromValue;
+
+  return path
+    .relative(process.cwd(), fromValue)
+    .split(path.sep)
+    .join("/");
+}
+
+function errorMessageReducer(message, { severity, line, column, text, rule }) {
+  return severity === "error"
+    ? `${message}\n   ✖   ${rule} ${line}:${column}   ${text}`
+    : message;
+}
+
+function errorResultReducer(message, result) {
+  return result.errored
+    ? result.warnings.reduce(
+        errorMessageReducer,
+        `${message}\n\n${logFrom(result.source)}`
+      )
+    : message;
+}
+
+function errorFormatter(results) {
+  return results.reduce(errorResultReducer, "FAILURES:") + "\n\n";
+}
+
+module.exports = function sortedStringFormatter(results) {
+  return stringFormatter(results) + errorFormatter(results);
+};
+
